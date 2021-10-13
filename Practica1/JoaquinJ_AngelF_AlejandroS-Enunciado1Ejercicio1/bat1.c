1cc622bd8bd898ae25fb9bc0bf4de205178a5878
Joaquin <joaquin.jimenezl@estudiante.uam.es>
2021-10-13 19:16:31 +0200

    entrega 1

---
+++ Practica1/JoaquinJ_AngelF_AlejandroS-Enunciado1Ejercicio1/bat1.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "ulcg.h"
+// Librerías necesarias para LCGs
+#include "gdef.h"
+#include "unif01.h"
+// Librerías necesarias para la batería de tests
+#include "bbattery.h"
+
+#define M 2147483647
+#define A 16807
+#define C 0
+#define DEFAULT_SEED 559079219
+
+int main (int argc, char* argv[])
+{
+   int seed = (argc > 1) ?  atoi(argv[1]) : DEFAULT_SEED;
+   unif01_Gen *gen;
+   gen = ulcg_CreateLCG (M, A, C, seed);
+   bbattery_SmallCrush (gen);
+   ulcg_DeleteGen (gen);
+
+   printf("Nuestra semilla: %d\n", seed);
+
+   return 0;
+}
