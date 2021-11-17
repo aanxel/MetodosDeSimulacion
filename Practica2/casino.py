795503b8d7399bd5e19e7d3479fa01728278661f
aanxel <angelfragua1@gmail.com>
2021-11-17 15:46:09 +0100

    Recocido simulado primera versión

--- Practica2/casino.py
+++ Practica2/casino.py
@@ -18,6 +18,15 @@
 import numpy as np
 import numpy.random as npr
 
+import math
+import random
+import time
+import sys
+import matplotlib.pyplot as plt
+import simanneal
+from simanneal.anneal import time_string
+from IPython.display import clear_output
+
 
 class SimCasino:
     def __init__(self, n_dias=30, max_partidas=50, max_fichas=150,
@@ -93,3 +102,190 @@ class SimCasino:
         return (p_bancarrotas / n_simulaciones,
                 fichas_dia / n_simulaciones,
                 p_part_antes_bancarrota)
+
+class CasinoAnnealer(simanneal.Annealer):
+    def __init__(self, initial_state=[1/7]*7, n_simulaciones=10000,
+                 T_config={'L': 1}, stop_config={'p_acc': 0.1, 'k': 5},
+                 load_state=None):
+        self.T_config = T_config
+        self.stop_config = stop_config
+        self.n_simulaciones = n_simulaciones
+        self.casino = SimCasino(n_dias=1, max_partidas=np.inf)
+        super().__init__(initial_state=initial_state, load_state=load_state)
+        self.T_configuration()
+        self.reset_metrics()
+
+    def reset_metrics(self):
+        self.epochs = 0
+        self.T_hist = []
+        self.E_hist = []
+        self.accept_hist = []
+        self.improv_hist = []
+        self.steps_hist = []
+        self.stop_config['trials'] = 0
+        self.stop_config['accepts'] = 0
+        self.stop_config['improves'] = 0
+
+    def energy(self):
+        _, fichas, _ = self.casino.simular(n_simulaciones=self.n_simulaciones)
+        # Número medio de fichas ganadas
+        return -sum([x*y for x, y in enumerate(fichas/self.casino.n_dias)])
+
+    def move(self):
+        self.epochs += 1
+        desplazamiento = 0.2
+        a = random.randint(0, len(self.state) - 1)
+        resto_prob_prev = 1 - self.state[a]
+        # Incrementamos una probabiliad un porcentaje 
+        self.state[a] *= (1 + random.uniform(-desplazamiento, desplazamiento))
+        resto_prob_new = 1 - self.state[a]
+        # Actualizamos el resto de forma proporcional
+        for i, p in enumerate(self.state):
+            if i != a:
+                p = p/resto_prob_prev * resto_prob_new
+
+    def T_function(self):
+        if self.epochs % self.T_config['L'] == 0:
+            T = self.Tmax * self.T_config['alfa']**self.epochs
+        else:
+            T = self.Tmax * self.T_config['alfa']**(self.epochs - self.epochs % self.T_config['L'])
+        return T
+
+    def T_configuration(self):
+        Tfactor = -math.log(self.Tmax / self.Tmin)
+        self.T_config['alfa'] = math.exp(Tfactor / self.steps)
+
+    def stop_criterion(self):
+        if self.epochs % (self.T_config['L'] * self.stop_config['k']) == 0:
+            if self.stop_config['accepts'] / self.stop_config['trials'] < self.stop_config['p_acc']:
+                self.user_exit = True
+            self.stop_config['trials'] = 0
+            self.stop_config['accepts'] = 0
+            self.stop_config['improves'] = 0
+
+    def anneal(self, updates=None):
+        if updates:
+            self.updates = updates
+        ###
+        self.reset_metrics()
+        self.T_configuration()
+        ###
+        step = 0
+        self.start = time.time()
+
+        # Precompute factor for exponential cooling from Tmax to Tmin
+        if self.Tmin <= 0.0:
+            raise Exception('Exponential cooling requires a minimum "\
+                "temperature greater than zero.')
+
+        # Note initial state
+        T = self.Tmax
+        E = self.energy()
+        prevState = self.copy_state(self.state)
+        prevEnergy = E
+        self.best_state = self.copy_state(self.state)
+        self.best_energy = E
+        trials, accepts, improves = 0, 0, 0
+        if self.updates > 0:
+            updateWavelength = self.steps / self.updates
+            self.update(step, T, E, None, None)
+
+        # Attempt moves to new states
+        while step < self.steps and not self.user_exit:
+            step += 1
+            ### Actualización de temperatura
+            T = self.T_function()
+            ###
+            dE = self.move()
+            if dE is None:
+                E = self.energy()
+                dE = E - prevEnergy
+            else:
+                E += dE
+            trials += 1
+            self.stop_config['trials'] += 1
+            if dE > 0.0 and math.exp(-dE / T) < random.random():
+                # Restore previous state
+                self.state = self.copy_state(prevState)
+                E = prevEnergy
+            else:
+                # Accept new state and compare to best state
+                accepts += 1
+                self.stop_config['accepts'] += 1
+                if dE < 0.0:
+                    improves += 1
+                    self.stop_config['improves'] += 1
+                prevState = self.copy_state(self.state)
+                prevEnergy = E
+                if E < self.best_energy:
+                    self.best_state = self.copy_state(self.state)
+                    self.best_energy = E
+            if self.updates > 1:
+                if (step // updateWavelength) > ((step - 1) // updateWavelength):
+                    self.update(
+                        step, T, E, accepts / trials, improves / trials)
+                    trials, accepts, improves = 0, 0, 0
+            ###
+            self.T_hist.append(T)
+            self.E_hist.append(E)
+            self.stop_criterion()
+            ###
+
+        self.state = self.copy_state(self.best_state)
+        if self.save_state_on_exit:
+            self.save_state()
+
+        ###
+        print()
+        ###
+        # Return best state and energy
+        return self.best_state, self.best_energy
+
+    def plot_evolution(self):
+        if len(self.T_hist) < 2:
+            print('No hay suficientes datos para la representación')
+            return
+        # Plot temperature, energy, accept, improve
+        fig = plt.figure(figsize=(22, 10), constrained_layout=True)
+        gs = fig.add_gridspec(2, 2)
+        fig.suptitle('Evolución del algoritmo')
+        metrics = [(self.T_hist, 'Temperatura'),
+                   (self.E_hist, 'Energía'),
+                   (self.accept_hist, 'Aceptación'),
+                   (self.improv_hist, 'Mejora')]
+        for i in range(2):
+            for j in range(2):
+                atr = metrics[i * 2 + j]
+                f_ax = fig.add_subplot(gs[i, j])
+                f_ax.set_title(f'{atr[1]}(iteraciones)')
+                if atr[1] == 'Energía' or atr[1] == 'Temperatura':
+                    f_ax.plot(list(range(0, self.epochs)), atr[0])
+                    continue  
+                f_ax.plot(self.steps_hist, atr[0])
+        plt.show()
+        pass
+
+    def default_update(self, step, T, E, acceptance, improvement):
+        clear_output(wait=True)
+        self.accept_hist.append(acceptance)
+        self.improv_hist.append(improvement)
+        self.steps_hist.append(step)
+
+        elapsed = time.time() - self.start
+        header = (' Temperature        Energy    Accept   Improve     Elapsed'
+                  '   Remaining')
+        if step == 0:
+            print(header, file=sys.stdout)
+            print('%12.5f  %12.2f                      %s            ' %
+                  (T, E, time_string(elapsed)), file=sys.stdout)
+            sys.stdout.flush()
+        else:
+            remain = (self.steps - step) * (elapsed / step)
+            print(header)
+            print('%12.5f  %12.2f  %7.2f%%  %7.2f%%  %s  %s' %
+                  (T, E, 100.0 * acceptance, 100.0 * improvement,
+                   time_string(elapsed), time_string(remain)))
+            sys.stdout.flush()
+
+    def copy_state(self, state):
+        return state.copy()
